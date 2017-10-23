325a1ef79f90a348dd2676f239d708391ab36a02
lost273 <dnk273@gmail.com>
2017-10-23 22:03:41 +0300

    Initial commit

---
+++ ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using ClientFeatures.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ClientFeatures.Controllers {
+    public class HomeController : Controller {
+        public ViewResult MakeBooking() {
+            return View(new Appointment {
+                ClientName = "Adam",
+                Date = DateTime.Now.AddDays(2),
+                TermsAccepted = true
+            });
+        }
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment appt) {
+            return Json(appt, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
