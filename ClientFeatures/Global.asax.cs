fa27ddaa7e1b91aeec74d13546534b83ce61eb3b
lost273 <dnk273@gmail.com>
2017-10-24 13:18:15 +0300

    24-10-2017-13-18

--- ClientFeatures/Global.asax.cs
+++ ClientFeatures/Global.asax.cs
@@ -5,10 +5,16 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace ClientFeatures {
     public class MvcApplication : System.Web.HttpApplication {
         protected void Application_Start() {
+            DisplayModeProvider.Instance.Modes.Insert(0,
+                new DefaultDisplayMode("OperaTablet") {
+                    ContextCondition = (context => context.Request.UserAgent.IndexOf
+                        ("Opera Tablet", StringComparison.OrdinalIgnoreCase) >= 0)
+                });
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
